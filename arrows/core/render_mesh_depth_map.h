e4b5b767636402548c14854e22f4306380177d82
Matthieu Zins <matthieu.zins@kitware.com>
2018-11-21 17:56:04 +0100

    wip

--- arrows/core/render_mesh_depth_map.h
+++ arrows/core/render_mesh_depth_map.h
@@ -56,7 +56,7 @@ namespace core {
  */
 KWIVER_ALGO_CORE_EXPORT
 vital::image_container_sptr render_mesh_depth_map(kwiver::vital::mesh_sptr mesh,
-                                                  kwiver::vital::camera_perspective_sptr camera);
+                                                  vital::camera_perspective_sptr camera);
 
 
 /// This function renders a height map of a triangular mesh
